c4efe6b85b88a68144e3003fa4990fe9add1c89a
Nick Grealy <nickgrealy@gmail.com>
2019-07-15 13:30:19 +1000

    Added minutes played.

--- express.js
+++ express.js
@@ -22,27 +22,13 @@ API.getVanityUrl = username => get(`http://api.steampowered.com/ISteamUser/Resol
 API.getFriends = steamId => get(`http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=${steamApiKey}&steamid=${steamId}&relationship=friend`)
 
 API.getFriendSummaries = steamIds => get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamApiKey}&steamids=${steamIds.join(',')}`)
-API.getGames = steamId => get(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamApiKey}&steamid=${steamId}&include_appinfo=1&include_played_free_games=1`)
+API.getOwnedGames = steamId => get(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${steamApiKey}&steamid=${steamId}&include_appinfo=1&include_played_free_games=1`)
 
-// API.getFriendSummaries = function(steamIds) {
-//     // response.response.players (array)
-//     return request({
-//         url: 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' + steamApiKey + '&steamids=' + steamIds.join(','),
-//         json: true
-//     })
-// }
-// API.getGames = function(steamId) {
-//     // response.games (array)
-//     return request({
-//         url: 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + steamApiKey + '&steamid=' + steamId + '&include_appinfo=1&include_played_free_games=1',
-//         json: true
-//     });
-// }
-API.sendError = function(res) {
-    return function(err) {
-        res.status(400).send(err);
-    }
-}
+const reduce = (arr, field) => arr.reduce((map, obj) => (map[obj[field]] = obj, map), {})
+
+// playtime_forever
+
+API.sendError = res => err => res.status(400).send(err)
 
 /* ---- Routes ---- */
 
@@ -131,56 +117,65 @@ app.route('/friends/games')
         var friendIds = req.query.friendIds
         var promises = []
         promises.push(API.getFriendSummaries(friendIds))
-        friendIds.forEach(id => promises.push(API.getGames(id)))
+        friendIds.forEach(friendId => promises.push(API.getOwnedGames(friendId)))
 
         Promise.all(promises)
             .then(responses => {
+                    const [friendSummaries, ...ownedGames] = responses
                     // map of friends...
-                    var friendsBySteamId = responses[0].response.players.reduce(function(map, p) {
+                    var friendsBySteamId = friendSummaries.response.players.reduce(function(map, p) {
                         map[p.steamid] = p;
                         return map;
                     }, {});
                     var friends = friendIds.map(id => friendsBySteamId[id])
                         // map of games...
+                    var gamesByFriendId = {}
                     var gamesByAppId = {};
+                    var totalMinutesByAppId = {};
                     var gameOwnersByAppId = {};
-                    for (var i = 1; i < responses.length; i++) {
-                        var games = responses[i].response.games;
+                    ownedGames.forEach(({response: { game_count, games }}, idx) => {
                         if (typeof games === 'undefined') {
-                            console.error('games was undefined at index ' + i + ' of ' + responses.length, JSON.stringify(responses[i], null, 4));
+                            console.error(`games was undefined at index ${idx}`)
                         } else {
-                            var steamid = friendIds[i - 1];
-                            games.forEach(function(g) {
+                            var steamid = friendIds[idx];
+                            gamesByFriendId[steamid] = reduce(games, 'appid')
+                            games.forEach(g => {
+                                const { playtime_forever: playtimeForeverInMinutes } = g
                                 gamesByAppId[g.appid] = g;
+                                totalMinutesByAppId[g.appid] = (totalMinutesByAppId[g.appid] || 0) + playtimeForeverInMinutes
                                 var owners = gameOwnersByAppId[g.appid];
                                 if (typeof owners === 'undefined') {
-                                    owners = gameOwnersByAppId[g.appid] = [];
+                                    owners = gameOwnersByAppId[g.appid] = []
                                 }
-                                owners.push(steamid);
+                                owners.push(steamid)
                             });
                         }
-                    }
-
+                    })
                     addHeaders(res)
 
+                    const headers = ['Logo', 'Name', 'Count', 'Total Minutes Played']
+
                     res.write('<h1>Shared games...</h1><hr/>')
-                    res.write('<table>')
+                    res.write('<table class="sticky_thead">')
                     res.write('<thead>')
 
                     // avatars
-                    res.write('<tr><th></th><th></th><th>Avatar:</th><th>')
+                    res.write(`<tr>${headers.map(x => '<th></th>').join('')}<th>Avatar:</th><th>`)
                     res.write(friends.map(f => '<img src="' + f.avatarmedium + '"></img>').join('</th><th>'));
                     res.write('</th></tr>');
 
                     // nicknames
-                    res.write('<tr><th></th><th></th><th>Nickname:</th><th>')
+                    res.write(`<tr>${headers.map(x => '<th></th>').join('')}<th>Nickname:</th><th>`)
                     res.write(friends.map(f => '<a href="' + f.profileurl + '">' + f.personaname + '</a>').join('</th><th>'))
                     res.write('</th></tr>');
 
                     // realnames
-                    res.write('<tr><th></th><th></th><th>Realname:</th><th>')
+                    res.write(`<tr>${headers.map(x => `<th></th>`).join('')}<th>Realname:</th><th>`)
                     res.write(friends.map(f => '<a href="' + f.profileurl + '">' + f.realname + '</a>').join('</th><th>'))
                     res.write('</th></tr>');
+
+                    // game headers
+                    res.write(`<tr>${headers.map(x => `<th>${x}</th>`).join('')}<th></th><th></th><th></th></tr>`)
 
                     res.write('</thead><tbody>')
 
@@ -190,16 +185,24 @@ app.route('/friends/games')
                     }
 
                     // games
-                    Object.keys(gamesByAppId).sort((a, b) => countFn(b) - countFn(a)).forEach(appid => {
+                    Object.keys(gamesByAppId).sort((a, b) => {
+                        return countFn(b) - countFn(a) || totalMinutesByAppId[b] - totalMinutesByAppId[a]
+                    }).forEach(appid => {
                         var game = gamesByAppId[appid];
                         var owners = gameOwnersByAppId[appid];
                         res.write('<tr>')
                         res.write('<th><img title="' + game.name + '" src="http://media.steampowered.com/steamcommunity/public/images/apps/' + appid + '/' + game.img_logo_url + '.jpg"></img></th>');
                         res.write('<th>' + game.name + '</th>');
                         res.write('<th>' + countFn(appid) + '</th>');
+                        res.write('<th>' + totalMinutesByAppId[appid] + '</th>');
+                        res.write('<th class="blue"></th>')
                         // res.write(`<th>`);
                         // and whether the user is an owner...
-                        res.write(friendIds.map(id => `<th class="${(owners.indexOf(id) !== -1 ? 'green' : '')}">` + (owners.indexOf(id) !== -1 ? 'TRUE' : '') + `</th>`).join(''));
+                        const renderUserCell = (friendId, gameId) => {
+                            const game = gamesByFriendId[friendId][gameId]
+                            return `<th class="${(owners.indexOf(friendId) !== -1 ? 'green' : '')}">` + (game ? game.playtime_forever : '') + `</th>`
+                        }
+                        res.write(friendIds.map(friendId => renderUserCell(friendId, appid)).join(''));
                         res.write('</tr>');
                     });
                     res.write('</tbody></table>');
